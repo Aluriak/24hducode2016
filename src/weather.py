9c31ec096278d0b41a1b48ac4c6d292191889221
Nedgang <babarouman@hotmail.fr>
2016-01-23 20:12:22 +0100

    Add prediction weather, and is_predictable

--- src/weather.py
+++ src/weather.py
@@ -7,6 +7,7 @@ latitude.
 ##########
 # IMPORT #
 ##########
+import datetime
 import requests
 
 #############
@@ -25,15 +26,38 @@ def actual_weather(latitude, longitude):
     web_data = requests.get("http://api.openweathermap.org/data/2.5/weather?lat="
                            + latitude +"&lon="+ longitude +
                            "&appid="+api_id).json()
-    # weather.json() -> dico
+    # web_data.json() -> dico
 
     weather = {}
     weather["description"] = web_data['weather'][0]['description']
     weather["humidity (%)"] = web_data['main']['humidity']
-    weather["wind (m/s)"] = web_data['wind']['speed']
-    weather["temperature (°C)"] = kelvin_to_celsius(web_data['main']['temp'])
+    weather["wind (km/h)"] = round(mps_to_kmph(web_data['wind']['speed']),2)
+    weather["temperature (°C)"] = round(kelvin_to_celsius(web_data['main']['temp']),2)
 
-    print(weather)
+    return weather
+
+def predicted_weather(latitude, longitude):
+    """
+    Return the predicted weather for the next 5 days.
+    """
+    api_id = 'f88e4f9c0329490d11901f8ff47777df'
+
+    latitude = str(latitude)
+    longitude = str(longitude)
+
+    web_data = requests.get("http://api.openweathermap.org/data/2.5/forecast?lat="
+                           + latitude +"&lon="+ longitude +
+                           "&appid="+api_id).json()['list']
+    predicted_weather = {}
+    for day in web_data:
+        if day['dt_txt'][-8:] == "15:00:00":
+            predicted_weather[day['dt_txt'][:10]] = {
+                'description': day['weather'][0]['description'],
+                'humidity (%)': day['main']['humidity'],
+                "wind (km/h)": round(mps_to_kmph(day['wind']["speed"]),2),
+                "temperature (°C)": round(kelvin_to_celsius(day['main']['temp']),2)}
+
+    return predicted_weather
 
 
 def kelvin_to_celsius(kelvin):
@@ -42,4 +66,23 @@ def kelvin_to_celsius(kelvin):
     """
     return kelvin-273.15
 
-actual_weather(46.5798114,0.34189)
+def mps_to_kmph(mps):
+    """
+    Convert meter/s to km/h
+    """
+    return mps*3.6
+
+def is_predictable(year, month, day):
+    """
+    Check if at the date, the weather can be predict.
+    Output: boolean
+    """
+    request = datetime.date(year, month, day)
+    today = datetime.date.today()
+    if request.toordinal() - today.toordinal() >= 5:
+        return True
+    else:
+        return False
+
+
+predicted_weather(46.5798114,0.34189)
