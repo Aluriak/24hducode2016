4517366bfccede33402706cc9f8394657612f07a
Aluriak <lucas.bourneuf@laposte.net>
2016-01-24 00:46:29 +0100

    Revert "deleted __init__"
    This reverts commit 5ae92346ffd925a41ea95d8fcc0a9afcde9c7b26.

---
+++ src/source_tourinsoft/__init__.py
@@ -0,0 +1,6 @@
+from .getDataFromTourinSoft import request
+from .SourceTourinsoft import SourceTourinsoft
+
+
+
+# 'Prestation' = # (Cours, Location de matériel, Pratique libre, Accompagnement, Stage d’initiation, Baptême)
