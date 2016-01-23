a5ebc1a73354c6e8ffb4d2b97e368c64babd3b18
Aluriak <lucas.bourneuf@laposte.net>
2016-01-24 00:30:07 +0100

    first pipeline for all data

--- src/__main__.py
+++ src/__main__.py
@@ -2,10 +2,21 @@
 Entry point for the project.
 
 """
+import sys
 
-# importing tests
-import matplotlib
-import plotly
+# from . import tourinsoft_api
+from . import tobject_factory
+from . import source_factory
 
 
+print('Init Sources factory…', end='') ; sys.stdout.flush()
+sources = source_factory.all_sources()
+print('OK !\nInit TObjects factory…', end='') ; sys.stdout.flush()
+tobjects_factory = tobject_factory.TObjectFactory(sources)
+print('OK !\nRequest JSON data…', end='') ; sys.stdout.flush()
+json = tourinsoft_api.request()
+print('OK !\nProduce TObjects…', end='') ; sys.stdout.flush()
+tobjects = tobjects_factory.tobjects(json)
+print('OK !')
 
+print(tobjects)
