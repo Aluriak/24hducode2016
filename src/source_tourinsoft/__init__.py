5ae92346ffd925a41ea95d8fcc0a9afcde9c7b26
RomainFeron <romain.feron@evobio.eu>
2016-01-24 00:44:46 +0100

    deleted __init__

--- src/source_tourinsoft/__init__.py
+++
@@ -1,6 +0,0 @@
-from .getDataFromTourinSoft import request
-from .SourceTourinsoft import SourceTourinsoft
-
-
-
-# 'Prestation' = # (Cours, Location de matériel, Pratique libre, Accompagnement, Stage d’initiation, Baptême)
