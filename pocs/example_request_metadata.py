d19b2d873bcbdc99c63ca89be69365fafd9f0f55
RomainFeron <romain.feron@evobio.eu>
2016-01-23 14:57:54 +0100

    Modified request function to be more general

--- pocs/example_request_metadata.py
+++ pocs/example_request_metadata.py
@@ -1,18 +1,31 @@
 import requests
+import json
 
-BASE_EVENTS = ('http://wcf.tourinsoft.com/Syndication/3.0/cdt72/e9a8e2bf-' +
-               'c933-4831-9ebb-87eec559a21a/')
+"""
+Example of request function to be used on tourinsoft API
+Input : base (events or places)
 
-BASE_PLACES = ('http://wcf.tourinsoft.com/Syndication/3.0/cdt72/969e24f9' +
-               '-75a2-4cc6-a46c-db1f6ebbfe97/')
+"""
 
-# FLAGS = 'format=json&metadata'
-FLAGS = 'format=json'
+URL = 'http://wcf.tourinsoft.com/Syndication/3.0/cdt72/'
+
+EVENT_URL = 'e9a8e2bf-c933-4831-9ebb-87eec559a21a/'
+
+PLACES_URL = '969e24f9-75a2-4cc6-a46c-db1f6ebbfe97/'
+
+# Parameters are stored as a dictionary
+parameters = {}
 
 
-def request():
-    # r = requests.get(BASE_EVENTS + collection + '$' + FLAGS)
-    r = requests.get(BASE_EVENTS + '$' + FLAGS)
-    print(r.json())
+def request(collection, parameters={}, metadata=True, event=True):
+    param_str = ('&' + par + '=' + parameters[par] for par in parameters)
+    url = URL + ''.join((EVENT_URL if event else PLACES_URL,
+                        collection,
+                         '?$format=json',
+                         ''.join(param_str),
+                         '&metadata' if metadata else ''))
+    print(url)
+    return requests.get(url).json()
 
-request()
+test = request('Objects')
+json.dump(test, open('test.json', 'w'))
