1e33bc2e4b0c4c2c61a891d5e77f6032e59ca64e
RomainFeron <romain.feron@evobio.eu>
2016-01-23 23:05:22 +0100

    Changes to defaults, chose variables to be used first

--- src/default.py
+++ src/default.py
@@ -1,10 +1,20 @@
 """
-Definitions of various default values.
+Definitions of default values.
 
 """
 
-FIELD_NAME = 'SyndicObjectName'
-FIELD_COORDINATES = 'Coordinates'  # Is a 2-tuple (Latitude, Longitude)
+# Following fields are found in Events and Places
+FIELD_NAME = 'ObjectName'  # String
+FIELD_TYPE = 'Type'  # String
+FIELD_UPDATED = 'Updated'  # Date
+FIELD_GEOGRAPHY = 'Geography'  # Class containing all geograhpical data
+
+# Following fields are found only in Places
+FIELD_OPENING = 'OpeningTimes'  # Class containing time
+FIELD_FREE = 'Free'  # Boolean to know if it's free
+
+# Distinguish between an Event and a Place
+FIELD_EVENT = 'Event'  # Boolean True if Event, False if Place
 
 DESCRIPTION = 'description'
 URL = 'url'
