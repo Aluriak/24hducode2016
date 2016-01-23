6b0bc7c6af4f7d26970d92ae5feb76b8c9d8b113
Nedgang <babarouman@hotmail.fr>
2016-01-23 23:35:58 +0100

    Changes to default

--- src/default.py
+++ src/default.py
@@ -4,4 +4,9 @@ Definitions of various default values.
 """
 
 FIELD_NAME = 'SyndicObjectName'
-FIELD_COORDINATES = 'Coordinates'  # Is a 2-tuple (Latitude, Longitude)
+FIELD_COORDINATES = 'Coordinates'  # 2-tuple (Latitude, Longitude)
+
+FIELD_WEATHER_NOW = 'WeatherNow'  # dict {weaver data}
+FIELD_WEATHER_PREDICTED = 'WeatherPredicted'  # dict {day number:{weaver data}}
+
+FIELD_DATE = 'EventDate'  # datetime object
