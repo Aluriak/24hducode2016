793bc117ed59d7b2bc65a337f478ba9402d75ce2
pierre <PSV@caramail.fr>
2016-01-23 21:12:00 +0100

    add wikipedia parser; deal with it; enkulé

--- src/default.py
+++ src/default.py
@@ -5,3 +5,6 @@ Definitions of various default values.
 
 FIELD_NAME = 'SyndicObjectName'
 FIELD_COORDINATES = 'Coordinates'  # Is a 2-tuple (Latitude, Longitude)
+
+DESCRIPTION = 'description'
+URL = 'url'
