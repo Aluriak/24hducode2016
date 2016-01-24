5acf20bf3b58ec5cc90a18199453db968bf2807c
Aluriak <lucas.bourneuf@laposte.net>
2016-01-24 01:07:36 +0100

    weather: fix typo + fix API usage

--- src/source_weather/source_weather.py
+++ src/source_weather/source_weather.py
@@ -3,6 +3,7 @@ Definition of a source than add dumb data
 
 """
 from src.source import Source
+from src import default
 from . import weather
 
 class SourceWeather(Source):
@@ -13,8 +14,9 @@ class SourceWeather(Source):
     """
 
     def enrichment(self, data_dict):
-        if default.FIELD_COORDINATES in data_dict:
-            lat, lon = data_dict[default.FIELD_COORDINATES]
+        if default.FIELD_LATITUDE in data_dict:
+            lat = data_dict[default.FIELD_LATITUDE]
+            lon = data_dict[default.FIELD_LONGITUDE]
             data_dict[default.FIELD_WEATHER] = weather.actual(lat, lon)
         if default.FIELD_DATE in data_dict:
             date = data_dict[default.FIELD_DATE]
