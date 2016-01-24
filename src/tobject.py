08378a59fcdeced9eb58d1721827da3e09657954
Aluriak <lucas.bourneuf@laposte.net>
2016-01-24 04:03:12 +0100

    implementation of tobject ; dict holder + proxy on databases

---
+++ src/tobject.py
@@ -0,0 +1,26 @@
+"""
+Definition of the TObject class
+
+"""
+
+
+
+class TObject:
+    """Wrapping dictionnary, generating data only if necessary"""
+
+    sources = []
+
+    def __init__(self, data_dict):
+        self.data = data_dict
+
+    def __getitem__(self, key):
+        if key not in self.data:
+            for source in TObject.sources:
+                if key in source.keywords:
+                    source.enrichment(self.data)
+                if key in self.data:
+                    break
+        return self.data[key]
+
+    def __str__(self):
+        return str(self.data)
