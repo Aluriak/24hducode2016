ad66b0c5b1ff52cddbfe1741a44721dce61ddb4b
Aluriak <lucas.bourneuf@laposte.net>
2016-01-24 01:45:04 +0100

    put normalization function in dedicated file

---
+++ src/source_tourinsoft/normalization.py
@@ -0,0 +1,48 @@
+"""
+Normalization of the data.
+
+Define mainly a dict {key in turinsoft: (normalized key, type)}.
+Define also a method that turn a dict with turinsoft keys into
+a dict of normalized keys.
+
+"""
+from functools import partial
+from datetime import datetime
+from src import default
+
+
+# Types definition
+TOURINFO_DATE_FORMAT = '%Y-%m-%dT%H:%M:%S'
+def date_type_wrapped(string, format):
+    """Wrapper around datetime, allowing usage of keywords arguments"""
+    return datetime.strptime(string, format)
+date_type = partial(date_type_wrapped, format=TOURINFO_DATE_FORMAT)
+
+
+# Dirty keys: (normalized key, type converter)
+KEYS_ASSOCIATION = {
+    'SyndicObjectID'   : (default.FIELD_ID, str),
+    'SyndicObjectName' : (default.FIELD_NAME, str),
+    'ObjectTypeName'   : (default.FIELD_TYPE, str),
+    'Updated'          : (default.FIELD_UPDATED, date_type),
+    'GmapLatitude'     : (default.FIELD_LATITUDE, str),
+    'GmapLongitude'    : (default.FIELD_LONGITUDE, str),
+    'Commune'          : (default.FIELD_CITY, str),
+    'CodePostal'       : (default.FIELD_ZIPCODE, int),
+    'OuvertureGranule' : (default.FIELD_OPENING, str),
+    'TarifGratuit'     : (default.FIELD_FREE, bool),
+}
+
+
+def normalized_keys(data_dict, isevent):
+    """Return a data_dict, equivalent to given one, but with normalized
+    keys and values"""
+    norm_data = {default.FIELD_EVENT: bool(isevent)}
+    # common data
+    for unkey, (normkey, type_conv) in KEYS_ASSOCIATION.items():
+        print('LOOP:', unkey, (normkey, type_conv))
+        if unkey in data_dict:
+            print('INLOOP:', type_conv, data_dict[unkey])
+            print('INLOOP:', type_conv(data_dict[unkey]))
+            norm_data[normkey] = type_conv(data_dict[unkey])
+    return norm_data
