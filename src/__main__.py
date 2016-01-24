1e2bcf929ae7a4b6df72ad2ddbbc96d613c7b0b1
Aluriak <lucas.bourneuf@laposte.net>
2016-01-24 01:08:10 +0100

    main: spawn tobjects from tourinsoft, enrich them with sources, and print them

--- src/__main__.py
+++ src/__main__.py
@@ -7,16 +7,16 @@ import sys
 # from . import tourinsoft_api
 from . import tobject_factory
 from . import source_factory
-
+from . import source_tourinsoft
 
 print('Init Sources factory…', end='') ; sys.stdout.flush()
 sources = source_factory.all_sources()
 print('OK !\nInit TObjects factory…', end='') ; sys.stdout.flush()
 tobjects_factory = tobject_factory.TObjectFactory(sources)
 print('OK !\nRequest JSON data…', end='') ; sys.stdout.flush()
-json = tourinsoft_api.request()
+json = source_tourinsoft.SourceTourinsoft().spawn_all()
 print('OK !\nProduce TObjects…', end='') ; sys.stdout.flush()
 tobjects = tobjects_factory.tobjects(json)
 print('OK !')
 
-print(tobjects)
+print(tuple(tobjects))
